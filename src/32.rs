2257e2d601f87816522c23bfcb3052193dd8dcec
duquesarfaraz601 <duquesarfaraz601@gmail.com>
2025-04-24 02:58:16 +0000

    Add 32.rs

---
+++ src/32.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut message = String::from("Hello, World!");
+
+    // Add some more content to the message
+    message.push_str(", this is something extra.");
+
+    println!("{}", message);
+}
